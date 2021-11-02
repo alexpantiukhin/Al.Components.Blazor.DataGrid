a4a32f1249207cac42a2bb2806654b81f595f512
Aleksandr Pantiukhin <pantjuxin-av@yandex.ru>
2021-11-02 20:27:49 +0300

    123

--- Al.Components.Blazor.DataGrid.Model/ColumnModel.cs
+++ Al.Components.Blazor.DataGrid.Model/ColumnModel.cs
@@ -19,39 +19,84 @@ namespace Al.Components.Blazor.DataGrid.Model
         //public event Func<Task> OnChange;
         //public event Func<Task> OnDragStarted;
         //public event Func<Task> OnDragEnded;
+        /// <summary>
+        /// Срабатывает после изменения ширины столбца
+        /// </summary>
         public event Func<Task>? OnWidthChanged;
 
         public const int MinWidth = 50;
         public const int DefaultWidth = 130;
+        /// <summary>
+        /// Видимость
+        /// </summary>
         public bool Visible { get; set; } = true;
+        /// <summary>
+        /// Возможность сортировки
+        /// </summary>
         public bool Sortable { get; set; }
+        /// <summary>
+        /// Ширина
+        /// </summary>
         public int Width { get; private set; } = DefaultWidth;
-        public string ShowName { get; set; }
+        /// <summary>
+        /// Отображаемый заголовок
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Направление сортировки
+        /// </summary>
         public ListSortDirection? Sort { get; set; }
+        /// <summary>
+        /// Возможность менять ширину
+        /// </summary>
         public bool Resizeable { get; set; }
+        /// <summary>
+        /// Фиксация столбца справа или слева
+        /// </summary>
         public ColumnFixedType FixedType { get; set; }
         /// <summary>
         /// Столбец можно перемещать
         /// </summary>
         public bool Draggable { get; set; }
+        /// <summary>
+        /// Возможность фильтровать по столбцу
+        /// </summary>
         public bool Filterable { get; set; }
+        /// <summary>
+        /// Выражение фильтра по столбцу
+        /// </summary>
         public FilterExpression<T> FilterExpression { get; set; }
+        /// <summary>
+        /// Узел сортируемой коллекции столбцов
+        /// </summary>
         public OrderableDictionaryNode<string, ColumnModel<T>> Node {get; private set;}
-        public Func<IQueryable<T>, bool, IQueryable<T>> AddSort { get; set; }
+        /// <summary>
+        /// Уникальное имя столбца
+        /// </summary>
         public string UniqueName { get; }
-        //public Func<IQueryable<T>, IOrderedQueryable<T>> AddFirstSort { get; init; }
-        //public Func<IOrderedQueryable<T>, IOrderedQueryable<T>> AddNextSort { get; init; }
-        //public Expression<Func<T, bool>> FilterExpression { get; set; }
         /// <summary>
         /// Выражение, уникально определяющее столбец
         /// </summary>
         public Expression<Func<T, object>> FieldExpression { get; }
+        /// <summary>
+        /// Тип поля столбца
+        /// </summary>
         public Type FieldType { get; }
         /// <summary>
         /// Флаг указывающий на то, что в текущий момент столбец перемещается
         /// </summary>
         public bool Dragging { get; private set; }
+        /// <summary>
+        /// Флаг, указывающий на то, что в данный момент столбец меняет ширину
+        /// </summary>
         public bool Resizing { get; private set; }
+        /// <summary>
+        /// Возвращает следующий за текущим столбец
+        /// </summary>
+        /// <returns>Null, если текущий - последний столбец</returns>
+        public ColumnModel<T>? Next =>
+            Node.Next?.Item;
+
 
         readonly MemberExpression MemberExpression;
 
@@ -121,13 +166,6 @@ namespace Al.Components.Blazor.DataGrid.Model
             return null;
         }
 
-        /// <summary>
-        /// Возвращает следующий за текущим столбец
-        /// </summary>
-        /// <returns>Null, если текущий - последний столбец</returns>
-        public ColumnModel<T>? Next =>
-            Node.Next?.Item;
-
         public void SetNode(OrderableDictionaryNode<string, ColumnModel<T>> node)
         {
             if (node == null)
@@ -171,6 +209,11 @@ namespace Al.Components.Blazor.DataGrid.Model
         //    return data.Where(FilterExpression);
         //}
 
+        /// <summary>
+        /// Изменить ширину
+        /// </summary>
+        /// <param name="width">Новая ширина</param>
+        /// <param name="notify">Уведомить об изменении ширины</param>
         public async Task WidthChange(int width, bool notify)
         {
             Width = width < MinWidth ? MinWidth : width;
@@ -179,9 +222,17 @@ namespace Al.Components.Blazor.DataGrid.Model
                 await OnWidthChanged.Invoke();
         }
 
+        /// <summary>
+        /// Получает значение поля указанного столбца для переданного экземпляра
+        /// </summary>
+        /// <param name="model">Экземпляр</param>
         public object? GetColumnValue(T model) =>
             FieldExpression?.Compile()?.Invoke(model);
 
+        /// <summary>
+        /// Применить пользовательские настройки
+        /// </summary>
+        /// <param name="settings">Настройки</param>
         public void ApplySetting(ColumnSettings<T> settings)
         {
             Sort = settings.Sort;
