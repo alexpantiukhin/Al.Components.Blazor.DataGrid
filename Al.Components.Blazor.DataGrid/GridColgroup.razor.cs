54f42888953005d7ff535411b98e2af69d85fc86
Aleksandr Pantiukhin <pantjuxin-av@yandex.ru>
2021-12-08 22:24:49 +0300

    1`2

--- Al.Components.Blazor.DataGrid/GridColgroup.razor.cs
+++ Al.Components.Blazor.DataGrid/GridColgroup.razor.cs
@@ -24,7 +24,7 @@ namespace Al.Components.Blazor.DataGrid
         {
             base.OnInitialized();
 
-            DataGridModel.Columns.OnResizeEnd += OnResizeStartHandler;
+            DataGridModel.Columns.OnResizing += OnResizeStartHandler;
         }
 
 
