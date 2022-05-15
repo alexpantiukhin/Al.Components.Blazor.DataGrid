8d531f876b416fff9be818e6d0a6ea28496d2f04
Alex <34450267+sawapont@users.noreply.github.com>
2022-05-15 23:03:45 +0300

    123

--- BlazorWasm/Program.cs
+++ BlazorWasm/Program.cs
@@ -1,4 +1,7 @@
+using Al.Components.Blazor.ResizeComponent;
+
 using BlazorWasm;
+using BlazorWasm.Data;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -6,6 +9,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddResizer();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
